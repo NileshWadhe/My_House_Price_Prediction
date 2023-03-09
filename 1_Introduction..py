cb1bc24109358c8fdc7c1cd31ac5e08d49e88a4b
Nilesh Wadhe <nilesh.wadhe05@gmail.com>
2023-03-10 01:17:57 +0530

    Hello World

---
+++ 1_Introduction..py
@@ -0,0 +1,295 @@
+#!/usr/bin/env python
+# coding: utf-8
+Addition program in python.This Master of Science in Data Science programme is designed for you to build a distinctive career in analytical and leadership roles in various sectors. With a perfect blend of machine learning, big data analytics, and statistics, the programme helps you gain experience in solving problems with real-world data.
+# # Data Science
+
+# ## Data Science
+
+# # Data Science
+
+# In[ ]:
+
+
+
+
+
+# a=10
+# b=20
+# c=a+b
+# print(c)
+
+# In[7]:
+
+
+a=10
+b=20
+c=a+b
+print(c)
+
+
+# In[10]:
+
+
+a=10
+b=5.5
+c=a+b
+print(c)
+
+
+# In[ ]:
+
+
+Hello
+
+
+# # How to run cells in Jupyter Notebook
+
+# In[ ]:
+
+
+Control+Enter= Used to execute current and selected cell.
+
+
+# In[13]:
+
+
+a=10
+b=5.5
+c=a+b
+print(c)
+
+
+# In[ ]:
+
+
+Shift+Enter= Used to execute current and selected cell and to select next cell
+
+
+# In[14]:
+
+
+a=10
+b=5.5
+c=a+b
+print(c)
+
+
+# In[ ]:
+
+
+alt+Enter= Used to execute current and it adds new blank cell
+
+
+# In[15]:
+
+
+a=10
+b=5.5
+c=a+b
+print(c)
+
+
+# In[ ]:
+
+
+shift+tab= To check functionality of function.
+
+
+# # Print function
+
+# In[16]:
+
+
+print("Data Science is great course of learning")
+
+
+# In[17]:
+
+
+print('Data Science is great course of learning')
+
+
+# In[18]:
+
+
+print("""Data Science is great course of learning""")
+
+
+# In[19]:
+
+
+x=100
+x
+
+
+# In[24]:
+
+
+x=100
+y=200
+z=300
+
+print(x)
+print(y)
+print(z)
+x
+y
+z
+
+
+# In[27]:
+
+
+print("Hello python")
+print("Data science")
+a=10
+b=20
+c="Machine Learning"
+b
+
+# print(a)
+# print(b)
+# print(c)
+
+
+
+# # Feature of Python
+
+# In[ ]:
+
+
+1)Easy to code-High level easy to code and developer friendly
+2)Free and Open Source- python language is freely available on official website.Anyone and create and contribute in its developement ie its open source.
+3)Python is portable/Platform independence-
+4)Dynamically Typed-
+5)Extensive Libraries- Pandas,Numpy,Matplotlib,seaborn,tensorflow,scipy,pytorche,scikitlearn.
+6)Support GUI- tkinter
+7)Embadable-Python we can use in C,C++ and JAVA.
+8)Interpreted-It is easy to debug in python but it makes python slow as compare to JAVA.
+
+
+# In[35]:
+
+
+a=10
+b=20
+add=a+b:
+print(add)
+
+
+# # Applications of Python
+
+# In[ ]:
+
+
+1) Web developement-Framework like Django,flask,pyramid.
+2) Game Developement- Pysoy,pygame and 3D game engine.
+3) AI (Artificial Inteligence) and ML- Anaylse,visualise and create models we have certain robust libraries in python.
+4)Web Scraping- Beautiful Soup.
+
+
+# In[ ]:
+
+
+int a=10;
+float b=5.5;
+
+
+# In[31]:
+
+
+a=5
+print(type(a))
+print(id(a))
+
+
+# In[ ]:
+
+
+type()
+id(a)
+
+
+# In[36]:
+
+
+a=10
+b=20
+c=3.14
+
+print("Welcome to DataScience")
+
+
+# In[44]:
+
+
+a=10
+print(a)
+b=20
+c=3.14
+a=45
+print(a)
+
+a=65
+print(a)
+print(b,type(b))
+print(c,type(c),id(c))
+
+print("Welcome to DataScience")
+
+
+# In[49]:
+
+
+print(10,20,30,"Data Science",sep=" * ")
+
+
+# In[50]:
+
+
+print(10,20,30,"Data Science",sep=" @ ")
+
+
+# In[64]:
+
+
+a=10
+b=20
+c=30
+d=50
+print(a,b,c,d,sep="\n")    
+
+
+# In[77]:
+
+
+print("Data Science",end="\n")
+print("Class",end=" ")
+print("Machine Learning")
+
+
+# In[81]:
+
+
+a=10
+
+
+a=a+50
+print(a)
+
+
+# In[89]:
+
+
+a=10
+a=45
+
+print(a,type(a),id(a))
+print(id(a))
+
+
+# In[ ]:
+
+
+
+
